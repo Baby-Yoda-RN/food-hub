5948e25404e4ce2f154830acdea54504a02a60b8
carizzuti <charlierizzuti@gmail.com>
2022-04-20 21:26:56 -0400

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  const message: string = 'Testing Text';
+
+  return <Text>{message}</Text>;
+};
+
+export default App;
