ea2db2244647fd5579e8bb30a722f8bbb61592b1
leo-he-1 <tuh28889@temple.edu>
2022-04-22 19:20:38 -0400

    added theme folder and changed button styles

--- mobile/src/components/button/Button.style.ts
+++ mobile/src/components/button/Button.style.ts
@@ -1,19 +1,20 @@
 import {StyleSheet} from 'react-native';
+import {color, size} from '../theme';
 
 export const styles = StyleSheet.create({
   container: {
     borderRadius: 28,
     alignItems: 'center',
     justifyContent: 'center',
-    width: 258,
-    height: 60,
+    width: '66%',
+    height: '07%',
   
   },
   buttonDisabled: {
-    backgroundColor: 'blue',
+    backgroundColor: color.lightGray,
   },
   buttonEnabled: {
-    backgroundColor: '#FE724C',
+    backgroundColor: color.primary,
   },
   title: {
     color: 'white',
