1e13b8f7c8f321e03826446207a531c4b8a135ae
leo-he-1 <tuh28889@temple.edu>
2022-04-22 22:04:04 -0400

    pulled main

---
+++ mobile/src/theme/color/color.ts
@@ -0,0 +1,12 @@
+import {Color} from './color.type';
+
+export const color: Color = {
+  primary: '#FE724C',
+  white: '#FFFFFF',
+  black: '#000000',
+  grayLow: '#E9E9E9',
+  grayMid: '#9EA1B1',
+  textOrange: '#C4C4C4',
+  textSearchBG: '#EFEFEF',
+  textRegularBG: '#E9E9E940',
+};
