bd0bbc897390c6ea6357ebc43b82b4de56886831
Dulaya Saennok <50040948+Dulaya@users.noreply.github.com>
2022-04-23 19:43:00 -0700

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Text} from 'react-native';
-import { SideMenu } from './src/components/sidemenu/SideMenu';
 import { AppNavigation } from './src/navigation/AppNavigation';
 
 const App = () => {
