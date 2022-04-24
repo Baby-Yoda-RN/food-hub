5b08d26b23d2c23c5c0844c59dcffbafe9fae63e
Dulaya Saennok <50040948+Dulaya@users.noreply.github.com>
2022-04-23 19:54:51 -0700

    Update App.tsx
    Clean up App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import {Text} from 'react-native';
 import { AppNavigation } from './src/navigation/AppNavigation';
 
 const App = () => {
-  const message: string = 'Testing Text';
   return (
-    <>
     <AppNavigation />
-    </>
   );
 };
 
