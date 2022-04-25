f06d87a7dccaa34e81cef464bbe19a38accfd07d
Michael Raynes <raynesmike@yahoo.com>
2022-04-25 14:14:18 -0700

    SplashScreen: v1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
 import {TextInput} from './src/components/textInput/TextInput';
 import {Icon} from './src/components';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   const message: string = 'Testing Text';
+  useEffect(() => {
+    setTimeout(() => {}, 1000);
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <Text>{message}</Text>
@@ -12,8 +17,8 @@ const App = () => {
       <TextInput type="password" />
       <TextInput type="search" />
       <TextInput type="phone" />
-      <Icon name="Heart" height={100} width={100} fill='orange' />
-      <Icon name="Bullet" height={10} width={10} fill='green' />
+      <Icon name="Heart" height={100} width={100} fill="orange" />
+      <Icon name="Bullet" height={10} width={10} fill="green" />
     </>
   );
 };
