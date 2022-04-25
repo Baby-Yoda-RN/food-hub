fe841bfd52f47a6ebea0515d4d739f8c1a77bdb4
Miguel Luna <36893258+mshago@users.noreply.github.com>
2022-04-25 17:17:02 -0400

    Update Badge.tsx

--- mobile/src/components/badge/Badge.tsx
+++ mobile/src/components/badge/Badge.tsx
@@ -4,6 +4,14 @@ import { BadgeProps } from './Badge.type'
 import { styles } from './Badge.style'
 
 export const Badge:FC<BadgeProps> = ({value, containerStyle, textStyle}) => {
+  
+  let newValue: string = '';
+  if(typeof value === 'number'){
+    newValue = value>99?'+99':value+''
+  }else if(typeof value === 'string'){
+    newValue = value
+  }
+  
   return (
     <View style={[styles.container,containerStyle]}>
       <Text style={[styles.text,textStyle]}>{value}</Text>
