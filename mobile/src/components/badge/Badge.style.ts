128e3c461696c2ff03ba98160e999cad29b3e6f3
Miguel Luna <36893258+mshago@users.noreply.github.com>
2022-04-25 17:15:57 -0400

    Update Badge.style.ts

--- mobile/src/components/badge/Badge.style.ts
+++ mobile/src/components/badge/Badge.style.ts
@@ -3,8 +3,8 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   container:{
     borderRadius:8,
-    paddingVertical:2,
-    paddingHorizontal:8,
+    paddingVertical:1,
+    paddingHorizontal:6,
     alignItems:'center',
     justifyContent:'center',
     backgroundColor:'#FFC529',
