03f2ca7618ae66df9887f6323d4bdd57999b1b27
Miguel Luna <mlunav99@gmail.com>
2022-04-26 19:21:53 -0400

    added props

--- mobile/src/components/bottomTabNavigator/BottomTabNavigator.tsx
+++ mobile/src/components/bottomTabNavigator/BottomTabNavigator.tsx
@@ -1,49 +1,43 @@
 import {View, TouchableOpacity, Alert} from 'react-native';
 import React, {FC} from 'react';
 import {styles} from './BottomTabNavigator.styles';
-import {BottomTabNavigatorProps} from './BottomTabNavigator.type';
+import {TBottomTabNavigator} from './BottomTabNavigator.type';
 import { Badge } from '../badge/Badge';
 import {Icon} from '../icon/Icon';
 import { color, size } from '../../theme';
 
-const SIZE = size.lg;
-const PRIMARY_COLOR = color.primary;
 
-export const BottomTabNavigator: FC<BottomTabNavigatorProps> = ({}) => {
-
-  const handleOnPress = () => {
-    Alert.alert('Pressed')
-  }
+export const BottomTabNavigator: FC<TBottomTabNavigator> = ({iconSize = size.lg, fillPin = color.grayDisabled}) => {
 
   return (
     <View style={styles.container}>
       <View style={styles.tabContainer}>
-        <TouchableOpacity onPress={handleOnPress}>
-        <Icon name="Compass" height={SIZE} width={SIZE} fill={PRIMARY_COLOR} />
+        <TouchableOpacity>
+        <Icon name="Compass" height={iconSize} width={iconSize} fill={fillPin} />
         </TouchableOpacity>
         <Badge containerStyle={styles.badgeContainer} value={'2'} />
       </View>
       <View style={styles.tabContainer}>
-        <TouchableOpacity onPress={handleOnPress}>
-        <Icon name="Pin" height={SIZE} width={SIZE} />
+        <TouchableOpacity>
+        <Icon name="Pin" height={iconSize} width={iconSize} />
         </TouchableOpacity>
         <Badge containerStyle={styles.badgeContainer} value={20} />
       </View>
       <View style={styles.tabContainer}>
-        <TouchableOpacity onPress={handleOnPress}>
-        <Icon name="Bag" height={SIZE} width={SIZE}/>
+        <TouchableOpacity>
+        <Icon name="Bag" height={iconSize} width={iconSize}/>
         </TouchableOpacity>
         <Badge containerStyle={styles.badgeContainer} value={10} />
       </View>
       <View style={styles.tabContainer}>
-        <TouchableOpacity onPress={handleOnPress}>
-        <Icon name="Heart" height={SIZE} width={SIZE}/>
+        <TouchableOpacity>
+        <Icon name="Heart" height={iconSize} width={iconSize}/>
         </TouchableOpacity>
         <Badge containerStyle={styles.badgeContainer} value={1}/>
       </View>
       <View style={styles.tabContainer}>
-        <TouchableOpacity onPress={handleOnPress}>
-        <Icon name="Bell" height={SIZE} width={SIZE}/>
+        <TouchableOpacity>
+        <Icon name="Bell" height={iconSize} width={iconSize}/>
         </TouchableOpacity>
         <Badge containerStyle={styles.badgeContainer} value={3}/>
       </View>
--- mobile/src/components/bottomTabNavigator/BottomTabNavigator.type.ts
+++ mobile/src/components/bottomTabNavigator/BottomTabNavigator.type.ts
@@ -1,4 +1,5 @@
 export type TBottomTabNavigator = {
-
+  iconSize?:number,
+  fillPin?:string,
 }
 
