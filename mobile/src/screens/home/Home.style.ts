f798826a4549776ed11d19729743fc50d3ca29b4
Miguel Luna <mlunav99@gmail.com>
2022-04-28 18:49:32 -0400

    home screen wip

--- mobile/src/screens/home/Home.style.ts
+++ mobile/src/screens/home/Home.style.ts
@@ -1,10 +1,26 @@
 import { StyleSheet } from "react-native";
-import { color, size } from "../../theme";
+import { color, font, size } from "../../theme";
 
 export const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:color.white,
-    paddingHorizontal:size.rg
+  },
+  scrollView:{
+    marginTop:size.rg
+  },
+  horizontalContainer:{
+    flexDirection:'row',
+  },
+  foodCardContainer:{
+    flex:1,
+    flexWrap:'wrap',
+  },
+  title:{
+    fontFamily:font.bold,
+    fontSize:size.lg,
+    color:'#323643',
+    marginRight:size.xl,
+    lineHeight:size.lg+10,
   }
 })
--- mobile/src/screens/home/Home.view.tsx
+++ mobile/src/screens/home/Home.view.tsx
@@ -1,10 +1,12 @@
 import React, {FC} from 'react';
-import {View, Text} from 'react-native';
-import {Card, Header, PinInput, TextInput} from '../../components';
+import {View, Text, ScrollView} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Header, TextInput} from '../../components';
+import {BottomTabNavigator} from '../../components/bottomTabNavigator/BottomTabNavigator';
 import {FoodCard} from '../../components/card/foodCard';
 import {RestaurantCard} from '../../components/card/restaurantCard';
 import {size, color} from '../../theme';
-import {TRestaurant} from '../../types/data';
+import {TFoodItem, TRestaurant} from '../../types/data';
 import {styles} from './Home.style';
 import {THomeView} from './Home.type';
 
@@ -20,24 +22,62 @@ const restaurantDummy: TRestaurant = {
     'https://www.tasteofhome.com/wp-content/uploads/2021/03/mcdonalds-1200x630-GettyImages-1285446341.jpg',
 };
 
+const foodDummy: TFoodItem = {
+  uuid: '12345',
+  name: 'Vegan Pizza',
+  description: 'Pizza para jotos',
+  rating: 5,
+  usersVoted: 100,
+  price: 5.5,
+  favorite: false,
+  imageName: 'https://foodish-api.herokuapp.com/images/pizza/pizza29.jpg',
+};
+
 export const HomeView: FC<THomeView> = () => {
   return (
-    <View style={styles.container}>
-      {/* <Header title='Hello' leftIconName='Chevron' containerStyle={{backgroundColor:color.primary}}/> */}
-      <Text>What would you like to order</Text>
-      <TextInput leftIcon="Bag" type="search" placeholder="Find food by name" />
-      <View style={{flexDirection: 'row'}}>
-        <Text>Featured Restaurants</Text>
-        <Text>View All</Text>
-      </View>
-      <View>
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-      </View>
-      <Text>Popular Items</Text>
-      <FoodCard />
-      <FoodCard />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header
+        deliveryLocation="4102 Pretty View Lane"
+        leftIconName="Hamburger"
+        iconHeight={20}
+        iconWidth={20}
+        rightIconName="Hamburger"
+        containerStyle={{marginHorizontal: size.rg, paddingBottom: size.rg}}
+      />
+      <ScrollView
+        style={{flex: 1, marginHorizontal:size.rg}}
+        nestedScrollEnabled
+        contentContainerStyle={styles.scrollView}>
+        <Text style={styles.title}>What would you like to order</Text>
+        <TextInput
+          leftIcon="Magnify"
+          type="search"
+          placeholder="Find food by name"
+        />
+        <View style={{flexDirection: 'row'}}>
+          <Text>Featured Restaurants</Text>
+          <Text>View All</Text>
+        </View>
+        <View>
+          <ScrollView
+            nestedScrollEnabled
+            horizontal
+            contentContainerStyle={[styles.horizontalContainer]}>
+            <RestaurantCard restaurant={restaurantDummy} />
+            <RestaurantCard restaurant={restaurantDummy} />
+            <RestaurantCard restaurant={restaurantDummy} />
+            <RestaurantCard restaurant={restaurantDummy} />
+          </ScrollView>
+        </View>
+        <Text>Popular Items</Text>
+        <View style={[styles.horizontalContainer, styles.foodCardContainer]}>
+          <FoodCard foodItem={foodDummy} />
+          <FoodCard foodItem={foodDummy} />
+          <FoodCard foodItem={foodDummy} />
+          <FoodCard foodItem={foodDummy} />
+        </View>
+      </ScrollView>
+      <BottomTabNavigator fillPin={color.primary} />
+    </SafeAreaView>
   );
 };
