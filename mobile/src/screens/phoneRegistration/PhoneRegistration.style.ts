68c643a3a73f1bdf3c6f5c860a9a38070976af1e
carizzuti <charlierizzuti@gmail.com>
2022-04-28 18:51:47 -0400

    update styles

--- mobile/src/screens/phoneRegistration/PhoneRegistration.style.ts
+++ mobile/src/screens/phoneRegistration/PhoneRegistration.style.ts
@@ -1,3 +1,30 @@
 import {StyleSheet} from 'react-native';
+import {size, color, font} from '../../theme';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    padding: size.lg,
+  },
+  headerContainer: {
+    paddingLeft: size.lg,
+    paddingTop: size.lg,
+  },
+  title: {
+    fontFamily: font.semiBold,
+    fontSize: size.lg,
+    color: color.black,
+    marginTop: size.rg,
+  },
+  subtitle: {
+    width: '65%',
+    marginBottom: size.lg,
+  },
+  buttonText: {
+    fontFamily: font.regular,
+  },
+  buttonContainer: {
+    marginTop: size.lg,
+    padding: size.rg,
+  },
+});
--- mobile/src/screens/phoneRegistration/PhoneRegistration.view.tsx
+++ mobile/src/screens/phoneRegistration/PhoneRegistration.view.tsx
@@ -1,16 +1,31 @@
 import React, {FC} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {Button, Header, TextInput} from '../../components';
+import {size} from '../../theme';
+import {styles} from './PhoneRegistration.style';
 import {TPhoneRegistrationViewProps} from './PhoneRegistration.type';
 
 export const PhoneRegistrationView: FC<TPhoneRegistrationViewProps> = () => {
   return (
-    <>
-      <Header />
-      <Text>REGISTRATION</Text>
-      <Text>Enter your phone number to verify your account</Text>
-      <TextInput type="phone" leftIcon="FlagUSA" />
-      <Button title="SEND" />
-    </>
+    <View>
+      <Header
+        leftIconName="ChevronLeft"
+        iconWidth={size.md}
+        iconHeight={size.md}
+        containerStyle={styles.headerContainer}
+      />
+      <View style={styles.container}>
+        <Text style={styles.title}>Registration</Text>
+        <Text style={styles.subtitle}>
+          Enter your phone number to verify your account
+        </Text>
+        <TextInput type="phone" leftIcon="FlagUSA" />
+        <Button
+          title="SEND"
+          containerStyle={styles.buttonContainer}
+          titleStyle={styles.buttonText}
+        />
+      </View>
+    </View>
   );
 };
