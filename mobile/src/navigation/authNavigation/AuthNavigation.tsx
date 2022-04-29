81ee8fbb9bcc14dd8c2971c512a0ffaabc44a592
Dulaya <dulaya.saennok@gmail.com>
2022-04-28 19:17:11 -0700

    remove default header

--- mobile/src/navigation/authNavigation/AuthNavigation.tsx
+++ mobile/src/navigation/authNavigation/AuthNavigation.tsx
@@ -7,7 +7,7 @@ const Stack = createStackNavigator<TAuthNavigationRoutes>();
 
 export const AuthNavigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown:false}}>
       <Stack.Screen
         name={EAuthNavigationRoutes.SIGNUP}
         component={SignUpScreen}
