1a36309a395d70d426f7b5557c3581d77f13a649
Miguel Luna <36893258+mshago@users.noreply.github.com>
2022-04-29 16:35:27 -0400

    Update Container.type.ts

--- mobile/src/components/container/Container.type.ts
+++ mobile/src/components/container/Container.type.ts
@@ -2,7 +2,6 @@ import { StyleProp, ViewStyle } from "react-native";
 
 export type TContainer = {
   isLoading?:boolean,
-  horizontal?:boolean,
   safeAreaStyle?: StyleProp<ViewStyle>,
   containerStyle?: StyleProp<ViewStyle>,
 }
