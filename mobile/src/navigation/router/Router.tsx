e68ec261ed50b8112188dc85b77f8d263431b89b
Michael Raynes <raynesmike@yahoo.com>
2022-05-02 13:35:44 -0700

    added Themes

--- mobile/src/navigation/router/Router.tsx
+++ mobile/src/navigation/router/Router.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import {AuthNavigation} from '../authNavigation/AuthNavigation';
 import {AppNavigation} from '../appNavigation/AppNavigation';
-
+import {AppTheme} from '../../theme/apptheme/AppTheme';
 export const Router = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={AppTheme.orange}>
       <AuthNavigation />
       {/* <AppNavigation /> */}
     </NavigationContainer>
---
+++ mobile/src/theme/apptheme/AppTheme.ts
@@ -0,0 +1,38 @@
+import {TAppTheme} from './AppTheme.type';
+import {color} from '../color/color';
+
+export const AppTheme = {
+  orange: {
+    dark: false,
+    colors: {
+      primary: color.primary,
+      background: color.white,
+      card: color.primary,
+      text: color.black,
+      border: color.primary,
+      notification: color.primary,
+    },
+  },
+  blue: {
+    dark: false,
+    colors: {
+      primary: color.blue,
+      background: color.white,
+      card: color.blue,
+      text: color.black,
+      border: color.blue,
+      notification: color.blue,
+    },
+  },
+  red: {
+    dark: false,
+    colors: {
+      primary: color.red,
+      background: color.white,
+      card: color.red,
+      text: color.black,
+      border: color.red,
+      notification: color.red,
+    },
+  },
+};
