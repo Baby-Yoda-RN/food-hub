8014d70faddf457e87c2db1aac00849cca93b528
Michael Raynes <raynesmike@yahoo.com>
2022-05-03 15:40:19 -0700

    added dark theme

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Router} from './src/navigation';
 import {NavigationContainer} from '@react-navigation/native';
+import {AppTheme} from './src/theme/apptheme/AppTheme';
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={AppTheme.white}>
       <Router />
     </NavigationContainer>
   );
--- mobile/src/theme/apptheme/AppTheme.ts
+++ mobile/src/theme/apptheme/AppTheme.ts
@@ -2,15 +2,26 @@ import {TAppTheme} from './AppTheme.type';
 import {color} from '../color/color';
 
 export const AppTheme = {
-  orange: {
+  dark: {
     dark: false,
     colors: {
       primary: color.primary,
-      background: color.white,
+      background: color.black,
       card: color.primary,
       text: color.black,
       border: color.primary,
       notification: color.primary,
+    },
+  },
+  white: {
+    dark: false,
+    colors: {
+      primary: color.blue,
+      background: color.white,
+      card: color.blue,
+      text: color.black,
+      border: color.blue,
+      notification: color.blue,
     },
   },
   blue: {
@@ -22,17 +33,6 @@ export const AppTheme = {
       text: color.black,
       border: color.blue,
       notification: color.blue,
-    },
-  },
-  red: {
-    dark: false,
-    colors: {
-      primary: color.red,
-      background: color.white,
-      card: color.red,
-      text: color.black,
-      border: color.red,
-      notification: color.red,
     },
   },
 };
