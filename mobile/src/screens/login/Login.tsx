37d0c144f05414098d17c29a50224bd60441e40e
Dulaya <dulaya.saennok@gmail.com>
2022-05-04 04:25:31 -0700

    condition render Login Screen or AppNavigation based on token

--- mobile/src/screens/login/Login.tsx
+++ mobile/src/screens/login/Login.tsx
@@ -1,18 +1,17 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC, useLayoutEffect, useState} from 'react';
 import {EAuthNavigationRoutes} from '../../navigation/authNavigation/AuthNavigation.type';
 import {TLoginNavigation, THandleLogin} from './Login.type';
 import {LoginScreenView} from './Login.view';
 import {foodHubAPI} from '../../config';
 import {storeToken, getToken} from '../../utilities';
+import {AppNavigation} from '../../navigation';
 
 export const LoginScreen: FC<TLoginNavigation> = ({navigation}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const [token, setToken] = useState<string>();
 
-  console.log(email, password);
-
-  const handleLogin:THandleLogin = async (email, password) => {
+  const handleLogin: THandleLogin = async (email, password) => {
     const response = await foodHubAPI.post('/login', {
       email,
       password,
@@ -20,7 +19,6 @@ export const LoginScreen: FC<TLoginNavigation> = ({navigation}) => {
 
     try {
       if (response.status === 200) {
-        console.log(response.data);
         if (response.data.token) {
           await storeToken('key', response.data.token);
           setToken(await getToken('key'));
@@ -31,28 +29,29 @@ export const LoginScreen: FC<TLoginNavigation> = ({navigation}) => {
     }
   };
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const getTokenFromLocalStorage = async () => {
       setToken(await getToken('key'));
     };
     getTokenFromLocalStorage();
-    console.log(token);
   }, []);
 
-  return (
-    <LoginScreenView
-      title="Login"
-      email={email}
-      setEmail={setEmail}
-      password={password}
-      setPassword={setPassword}
-      login={() => handleLogin(email, password)}
-      isLoading={false}
-      goToSignUp={() => navigation.navigate(EAuthNavigationRoutes.SIGNUP)}
-      goBack={() => navigation.goBack()}
-      goToResetPassword={() =>
-        navigation.navigate(EAuthNavigationRoutes.RESET_PASSWORD)
-      }
-    />
-  );
+  if (token) return <AppNavigation />;
+  else
+    return (
+      <LoginScreenView
+        title="Login"
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        login={() => handleLogin(email, password)}
+        isLoading={false}
+        goToSignUp={() => navigation.navigate(EAuthNavigationRoutes.SIGNUP)}
+        goBack={() => navigation.goBack()}
+        goToResetPassword={() =>
+          navigation.navigate(EAuthNavigationRoutes.RESET_PASSWORD)
+        }
+      />
+    );
 };
