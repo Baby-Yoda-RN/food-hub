107c47624c85445a391df850134ddbc3aa9210f6
leo-he-1 <tuh28889@temple.edu>
2022-05-05 18:56:50 -0400

    rebase

--- mobile/src/components/toggleButton/ToggleButton.styles.ts
+++ mobile/src/components/toggleButton/ToggleButton.styles.ts
@@ -1,23 +1,33 @@
-import { StyleSheet } from "react-native";
-import { color, size } from "../../theme";
+import {StyleSheet} from 'react-native';
+import {color, size} from '../../theme';
 
 export const styles = StyleSheet.create({
-    container: {
-        borderRadius: size.rg,
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        flexDirection: 'row',
-    },
-    buttonDisabled: {
-        backgroundColor: color.grayDisabled,
-    },
-    buttonEnabled: {
-        backgroundColor: color.primary,
-    },
-    title: {
-        color: color.white,
-        fontSize: size.rg,
-        lineHeight: size.rg,
-        textAlign: 'center',
-    },
+  container: {
+    backgroundColor: color.white,
+    borderRadius: size.xl,
+    borderWidth: 1,
+    borderColor: color.primary,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 2,
+  },
+  button: {
+    flex:1,
+    borderRadius: size.xl,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical:size.rg,
+  },
+  buttonDisabled: {
+    backgroundColor: color.grayDisabled,
+  },
+  buttonEnabled: {
+    backgroundColor: color.primary,
+  },
+  title: {
+    color: color.white,
+    fontSize: size.rg,
+    lineHeight: size.rg,
+    textAlign: 'center',
+  },
 });
