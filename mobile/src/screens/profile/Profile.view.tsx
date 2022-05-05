4874857b52ba85f5390c671a1ad1c5909a997f0d
JoseF <jose.tf2396@gmail.com>
2022-05-05 16:59:52 -0700

    containerized view

--- mobile/src/screens/profile/Profile.view.tsx
+++ mobile/src/screens/profile/Profile.view.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
-import {Header, TextInput} from '../../components';
+import {Header, TextInput, Container} from '../../components';
 import {size, color} from '../../theme';
 import {styles} from './Profile.style';
 import {TProfileViewProps} from './Profile.type';
@@ -12,34 +12,35 @@ export const ProfileView: FC<TProfileViewProps> = ({
   profileURL,
   goBack,
 }) => {
-
   return (
     <>
-      <View>
-        <Header
-          leftIconName="ChevronLeft"
-          iconWidth={size.rg}
-          iconHeight={size.rg}
-          containerStyle={styles.header}
-          leftIconStyle={{backgroundColor: color.white}}
-          leftPress={goBack}
-        />
-      </View>
-      <View style={styles.profileContainer}>
-        <Image style={[styles.image]} source={{uri: profileURL}} />
-        <Text style={styles.name}>{name}</Text>
-        <TouchableOpacity>
-          <Text>Edit Profile</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.text}>Full name</Text>
-        <TextInput type={'regular'} placeholder={name} />
-        <Text style={styles.text}>E-mail</Text>
-        <TextInput type={'regular'} placeholder={email} />
-        <Text style={styles.text}>Phone number</Text>
-        <TextInput type={'regular'} placeholder={phoneNumber} />
-      </View>
+      <Container
+        header={
+          <Header
+            leftIconName="ChevronLeft"
+            iconWidth={size.rg}
+            iconHeight={size.rg}
+            containerStyle={styles.header}
+            leftIconStyle={{backgroundColor: color.white}}
+            leftPress={goBack}
+          />
+        }>
+        <View style={styles.profileContainer}>
+          <Image style={[styles.image]} source={{uri: profileURL}} />
+          <Text style={styles.name}>{name}</Text>
+          <TouchableOpacity>
+            <Text>Edit Profile</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.text}>Full name</Text>
+          <TextInput type={'regular'} placeholder={name} />
+          <Text style={styles.text}>E-mail</Text>
+          <TextInput type={'regular'} placeholder={email} />
+          <Text style={styles.text}>Phone number</Text>
+          <TextInput type={'regular'} placeholder={phoneNumber} />
+        </View>
+      </Container>
     </>
   );
 };
