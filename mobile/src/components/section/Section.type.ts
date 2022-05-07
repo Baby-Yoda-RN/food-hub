336f8975a839be847191dc59a8e22a7090e99cb3
Miguel Luna <mlunav99@gmail.com>
2022-05-07 11:44:19 -0400

    section component

---
+++ mobile/src/components/section/Section.type.ts
@@ -0,0 +1,9 @@
+import { StyleProp, ViewStyle } from "react-native";
+import { size } from "../../theme";
+
+export type TSection = {
+  containerStyle?:StyleProp<ViewStyle>;
+  marginTop?: keyof typeof size;
+  marginBottom?: keyof typeof size;
+  marginHorizontal?: keyof typeof size;
+};
