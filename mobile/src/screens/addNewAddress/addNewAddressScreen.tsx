a1e74ed8ed9f51306b215ce7183e9d9b1621bcc2
JoseF <jose.tf2396@gmail.com>
2022-05-13 20:04:20 -0700

    first commit

--- mobile/src/screens/addNewAddress/addNewAddressScreen.tsx
+++ mobile/src/screens/addNewAddress/addNewAddressScreen.tsx
@@ -1,9 +1,20 @@
 import React, {FC} from 'react';
 import {TaddNewAddressNavigation} from './addNewAddressScreen.type';
 import {AddNewAddressScreenView} from './addNewAddressScreen.view';
+import {useGlobalState} from '../../context/global';
 
 export const AddNewAddressScreen: FC<TaddNewAddressNavigation> = ({
   navigation,
 }) => {
-  return <AddNewAddressScreenView goBack={() => navigation?.goBack()} />;
+  const {state} = useGlobalState();
+
+  return (
+    <AddNewAddressScreenView
+      goBack={() => navigation?.goBack()}
+      name={state.userInfo.name}
+      email={state.userInfo.email}
+      phoneNumber={state.userInfo.phone}
+      saveButton={() => {}}
+    />
+  );
 };
