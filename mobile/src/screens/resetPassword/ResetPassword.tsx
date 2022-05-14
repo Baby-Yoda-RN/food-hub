49027e38e3dbc29220c14419a35a48a79be02b3e
Michael Raynes <raynesmike@yahoo.com>
2022-05-13 20:46:02 -0700

    Fixed Require Cycle, Note, we need extended paths for inputs to avoid the import cycle

--- mobile/src/screens/resetPassword/ResetPassword.tsx
+++ mobile/src/screens/resetPassword/ResetPassword.tsx
@@ -1,12 +1,19 @@
-import React, {FC} from "react";
-import { EAuthNavigationRoutes } from "../../navigation";
-import { TResetPasswordNavigation, TResetPasswordProps } from "./ResetPassword.type";
-import { ResetPasswordView } from "./ResetPassword.view";
+import React, {FC} from 'react';
+import {EAuthNavigationRoutes} from '../../navigation/authNavigation/AuthNavigation.type';
+import {
+  TResetPasswordNavigation,
+  TResetPasswordProps,
+} from './ResetPassword.type';
+import {ResetPasswordView} from './ResetPassword.view';
 
-export const ResetPasswordScreen: FC<TResetPasswordNavigation> = ({navigation}) => {
-    return <ResetPasswordView 
-        title="Reset Password"
-        subtitle="Please enter your email address to request a password reset"
-        press={() => navigation.navigate(EAuthNavigationRoutes.LOGIN)}
+export const ResetPasswordScreen: FC<TResetPasswordNavigation> = ({
+  navigation,
+}) => {
+  return (
+    <ResetPasswordView
+      title="Reset Password"
+      subtitle="Please enter your email address to request a password reset"
+      press={() => navigation.navigate(EAuthNavigationRoutes.LOGIN)}
     />
-}
+  );
+};
