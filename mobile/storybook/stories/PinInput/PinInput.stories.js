66235b6d4e40be4c53094c2ced1751a9a33750e4
Alphi Henry <aahenry51@gmail.com>
2022-05-16 00:25:43 -0400

    PinInput Update

--- mobile/storybook/stories/PinInput/PinInput.stories.js
+++ mobile/storybook/stories/PinInput/PinInput.stories.js
@@ -3,22 +3,39 @@ import React from 'react';
 import {PinInput} from '../../../src/components';
 import CenterView from '../CenterView';
 import {Container, Section} from '../../../src/components';
+import {styles} from './PinInput.style';
 
 storiesOf('PinInput', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
   .add('Variants', () => (
     <Container isScrollViewDisabled={false}>
-      <Section>
-        <PinInput keyBoardType="numeric" digitCount={5} />
+      <Section style={styles.section}>
+        <PinInput
+          keyBoardType="numeric"
+          digitCount={5}
+          textInputStyle={styles.input}
+        />
       </Section>
-      <Section>
-        <PinInput keyBoardType="numeric" digitCount={4} />
+      <Section style={styles.section}>
+        <PinInput
+          keyBoardType="numeric"
+          digitCount={4}
+          textInputStyle={styles.input}
+        />
       </Section>
-      <Section>
-        <PinInput keyBoardType="numeric" digitCount={3} />
+      <Section style={styles.section}>
+        <PinInput
+          keyBoardType="numeric"
+          digitCount={3}
+          textInputStyle={styles.input}
+        />
       </Section>
-      <Section>
-        <PinInput keyBoardType="numeric" digitCount={2} />
+      <Section style={styles.section}>
+        <PinInput
+          keyBoardType="numeric"
+          digitCount={2}
+          textInputStyle={styles.input}
+        />
       </Section>
     </Container>
   ));
