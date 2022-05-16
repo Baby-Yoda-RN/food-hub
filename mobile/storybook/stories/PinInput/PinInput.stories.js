1f38d9a5cc113974cb68a56773c2796553b04ac0
Alphi Henry <aahenry51@gmail.com>
2022-05-16 00:13:06 -0400

    PinInputInit

--- mobile/storybook/stories/PinInput/PinInput.stories.js
+++ mobile/storybook/stories/PinInput/PinInput.stories.js
@@ -2,7 +2,23 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import {PinInput} from '../../../src/components';
 import CenterView from '../CenterView';
+import {Container, Section} from '../../../src/components';
 
 storiesOf('PinInput', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('Default', () => <PinInput keyBoardType="numeric" digitCount={4} />);
+  .add('Variants', () => (
+    <Container isScrollViewDisabled={false}>
+      <Section>
+        <PinInput keyBoardType="numeric" digitCount={5} />
+      </Section>
+      <Section>
+        <PinInput keyBoardType="numeric" digitCount={4} />
+      </Section>
+      <Section>
+        <PinInput keyBoardType="numeric" digitCount={3} />
+      </Section>
+      <Section>
+        <PinInput keyBoardType="numeric" digitCount={2} />
+      </Section>
+    </Container>
+  ));
