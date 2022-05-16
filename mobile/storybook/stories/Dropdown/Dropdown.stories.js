985128b4c5be0ab8875288202e0e7f1988d1c4fc
Alphi Henry <aahenry51@gmail.com>
2022-05-16 00:33:27 -0400

    dropdown

--- mobile/storybook/stories/Dropdown/Dropdown.stories.js
+++ mobile/storybook/stories/Dropdown/Dropdown.stories.js
@@ -2,10 +2,11 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import {Dropdown} from '../../../src/components';
 import CenterView from '../CenterView';
+import {Container, Section} from '../../../src/components';
 
 storiesOf('Dropdown', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('Default', () => (
+  .add('Variants', () => (
     <Dropdown
       data={[
         {label: 'Popular', value: 'popular'},
