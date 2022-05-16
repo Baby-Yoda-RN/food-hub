bbdba07d3954a2ec018f6f0a677cb2b4566c410e
Dulaya <dulaya.saennok@gmail.com>
2022-05-16 08:17:02 -0700

    Story Book: Buttons

--- mobile/storybook/stories/Button/Button.stories.js
+++ mobile/storybook/stories/Button/Button.stories.js
@@ -1,21 +1,47 @@
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react-native';
+import {storiesOf} from '@storybook/react-native';
 import React from 'react';
-import { Text } from 'react-native';
-import {Button} from '../../../src/components';
+import {Button, Container, Section} from '../../../src/components';
 import CenterView from '../CenterView';
+import {styles} from './Button.style';
+import {size, color} from '../../../src/theme';
 
 storiesOf('Button', module)
-  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('Default', () => (
-    <Button 
-    onPress={() => {}} 
-    title="Default"
-    />
-  ))
-  .add('with some emoji', () => (
-    <Button onPress={action('clicked-emoji')}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  .add('Variants', () => (
+    <Container containerStyle={styles.container}>
+      <Section style={styles.defaultContainer}>
+        <Button title="Default" containerStyle={styles.default} />
+        <Button
+          title="Default Light"
+          titleStyle={styles.lightText}
+          containerStyle={styles.light}
+        />
+      </Section>
+      <Section style={styles.logoContainer}>
+        <Button
+          containerStyle={styles.logo}
+          titleStyle={styles.logoText}
+          title="Google"
+          leftIconName="Google"
+          iconSize={size.lg}
+        />
+        <Button
+          containerStyle={styles.logo}
+          titleStyle={styles.logoText}
+          title="Facebook"
+          leftIconName="Facebook"
+          iconSize={size.lg}
+        />
+      </Section>
+      <Section style={styles.logoutContainer}>
+        <Button
+          containerStyle={styles.logoutLogo}
+          titleStyle={styles.logoutText}
+          title="Logout"
+          leftIconName="Logout"
+          leftIconStyle={styles.logoutContainer}
+          iconSize={size.rg}
+        />
+      </Section>
+    </Container>
   ));
