d56a3df54dca7be1efe8e5f57e796f34c8d5e8f0
Dulaya <dulaya.saennok@gmail.com>
2022-05-15 19:27:07 -0700

    Navigate to Phone Registration screen

--- mobile/src/screens/verify/Verify.type.ts
+++ mobile/src/screens/verify/Verify.type.ts
@@ -1,17 +1,12 @@
-import {StackScreenProps} from '@react-navigation/stack';
-import {
-  TAuthNavigationRoutes,
-  EAuthNavigationRoutes,
-  TAuthNavigation,
-} from '../../navigation';
+import { TAuthNavigation, } from '../../navigation';
 
 export type TVerifyScreenViewProps = {
   title: string;
   emailName: string;
   pinEntry: string;
-  onPressResend: () => {};
   goBack: () => void;
   onPressSubmit: () => void;
+  OnPressResend: () => void;
 };
 
 export type TVerifyScreenNavigation = {
